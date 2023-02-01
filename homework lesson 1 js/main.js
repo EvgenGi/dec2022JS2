f36b31135b3e87e2612b15ccf8d79a89622cef92
EvgenGi <alkarman@i.ua>
2023-02-01 12:49:04 +0200

    Initial commit

---
+++ homework lesson 1 js/main.js
@@ -0,0 +1,38 @@
+let hi = 'hello'
+console.log(hi);
+let o = 'owu'
+console.log(o);
+let com = 'com'
+console.log(com);
+let u = 'ua'
+console.log(u);
+let n = 1
+console.log(n);
+let nu = 10
+console.log(nu);
+let num = -999
+console.log(num);
+let numb = 123
+console.log(numb);
+let numbe = 3.14
+console.log(numbe);
+let number = 2.7
+console.log(number);
+let six = 16
+console.log(six);
+let bool = true
+console.log(bool);
+let boo = false;
+console.log(boo);
+
+let firstName = 'Evheniy';
+let middleName = 'Valeriyovich';
+let lastName = 'Moskalenko';
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+let a = 100
+console.log(typeof a)
+let b = '100'
+console.log(typeof b)
+let c = true
+console.log(typeof c)
